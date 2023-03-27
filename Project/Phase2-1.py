d8bb78baa16549cea0b981794ac9aed25ff5c9a1
SiFzl <126234558+SiFzl@users.noreply.github.com>
2023-03-27 19:17:52 +0330

    First update for 2-1

--- Project/Phase2-1.py
+++ Project/Phase2-1.py
@@ -4,7 +4,7 @@ import numpy as np
 # Load image
 img = cv2.imread("/Users/sina.fazel/Desktop/Python/Project/test2-1.png")
 
-# Convert to HSV color space
+# Convert to HSV
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # Define lower and upper bounds for red color
@@ -13,13 +13,19 @@ upper_red = np.array([10, 255, 255])
 lower_red2 = np.array([170, 50, 50])
 upper_red2 = np.array([180, 255, 255])
 
-# Create a mask for red color
+# Create a filter mask for red color
 mask_red1 = cv2.inRange(hsv, lower_red, upper_red)
 mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
 mask_red = mask_red1 + mask_red2
 
-# Find contours in the mask
-contours, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Apply a Gaussian blur to reduce noise
+blur = cv2.GaussianBlur(mask_red, (5, 5), 0)
+
+# Apply Canny edge detection
+edges = cv2.Canny(blur, 50, 150, apertureSize=3)
+
+# Find contours in the masked and filtered image
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # Draw contours and mark centers
 centers = []
@@ -33,7 +39,7 @@ for cnt in contours:
         cv2.circle(img, center, 3, (0, 0, 255), -1)
         centers.append(center)
 
-# Sort centers from left to right
+# Sort centers
 centers.sort(key=lambda c: c[0])
 
 # Print the first and last centers
@@ -44,6 +50,3 @@ print("Last center:", centers[-1])
 cv2.imshow("output", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
